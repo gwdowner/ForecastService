dccda2cbfcca375a739e6d06a6ac10a8b5825fd4
gwdowner <gwdowner@outlook.com>
2020-03-26 16:35:23 +0000

    Update forecast endpoint to return forecast data
    Still needs to be improved to return region data aswell

--- Server/forecast.py
+++ Server/forecast.py
@@ -11,17 +11,30 @@ modelData = BaseModel.loadModel('5e5e96dbff5c4a28f4a6367b')
 model = modelData['model']
 norm_stats = modelData['norm_data']
 
+def mergeForecastWithPrediction(a, b):
+    print(b)
+    return {
+        
+        'time':b['time'],
+        'solarMW':a[0]
+    }
+
 @forecastApi.route('/')
 def get_all():
     ## Tempory read in data ##
     uri = os.getenv('DATA_SERVICE_URL') 
-    data = [DataRequest.getData(uri)[0]]
+    rawData = DataRequest.getData('{uri}/api/data/forecast'.format(uri=uri))
+    # print(tmpData)
+    # data = [DataRequest.getData('{uri}/api/data/'.format(uri=uri))[0]]
+    # print(data)
     ## Delete above here once support for   ##
     ## future data is availible             ##
 
-    data = DataTransform.transform(data)
-    data.pop('solarMW')
+    data = DataTransform.transform(rawData)
     norm_data = DataTransform.normalise(data, norm_stats)
     prediction = model.predict(norm_data).tolist()
-    
-    return {'prediction':prediction[0]}
+    x = list(map(mergeForecastWithPrediction, prediction, rawData))
+    print(x)
+
+
+    return {'prediction':x}
